ffcd90c811cafeb371f3f2773813215ac1765b89
Rhys Powell <rhys@rpowell.me>
2017-08-25 09:58:08 +1000

    wip migration to bs-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    main: './lib/js/src/main.js'
-  },
+  entry: './src/Main.re',
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      // Set up Reason and OCaml files to use the loader
+      { test: /\.(re|ml)$/, use: 'bs-loader' }
+    ]
+  },
+  resolve: {
+    // Add .re and .ml to the list of extensions webpack recognizes
+    extensions: ['.re', '.ml', '.js']
   }
 };
