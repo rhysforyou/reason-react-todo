d3cce93e3f9079c2ac9011a0453ba8f2deb15a2b
Rhys Powell <rhys@rpowell.me>
2017-11-30 19:32:15 +1100

    remove bs-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,13 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  entry: './src/Main.re',
+  entry: './src/Main.bs.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'main.js'
-  },
-  module: {
-    rules: [
-      // Set up Reason and OCaml files to use the loader
-      { test: /\.(re|ml)$/, use: 'bs-loader' }
-    ]
   },
   resolve: {
     // Add .re and .ml to the list of extensions webpack recognizes
     extensions: ['.re', '.ml', '.js']
   }
-};
+}
